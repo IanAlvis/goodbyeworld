902d4d26a0568b3671ea2cbec50b1f5a8d94fa19
Ian Alvis <ioa002@email.latech.edu>
2024-02-16 09:54:00 -0600

    Added self destruction sequence

--- goodbyeworld.py
+++ goodbyeworld.py
@@ -1,5 +1,12 @@
-chat = 1
-while chat == 1:
-    text = input ("")
-    if text == "screaming in public restrooms prank":
-        print("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAHHHHH")
+from time import sleep
+print("goodbye world")
+sleep (5)
+goodbye = 1
+answer = 2
+timer = 2
+while goodbye == 1:
+    answer *= 2
+    print(answer)
+    sleep(timer)
+    if timer >= 0:
+        timer -= 0.1
